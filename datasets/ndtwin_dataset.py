26b04c524455167fffad6331fc4679ec306e2a9a
mossbee <hoangnhatdb@gmail.com>
2025-09-08 10:48:29 +0700

    update

--- datasets/ndtwin_dataset.py
+++ datasets/ndtwin_dataset.py
@@ -37,7 +37,7 @@ class NDTwinDataset(Dataset):
         image_id = self.image_id[item]
 
         # image
-        image = Image.open(os.path.join(DATAPATH, 'CelebA_HQ_448_Final', image_path[image_id])).convert('RGB')  # (C, H, W)
+        image = Image.open(os.path.join(DATAPATH, 'CelebA_HQ_448_Final/CelebA_HQ_448_Final', image_path[image_id])).convert('RGB')  # (C, H, W)
         image = self.transform(image)
 
         # return image and label
@@ -71,7 +71,7 @@ class NDTwinVerificationDataset(Dataset):
     def _load_image(self, img_path):
         """Load image with caching to handle duplicates efficiently"""
         if img_path not in self.image_cache:
-            full_path = os.path.join(self.data_path, 'CelebA_HQ_448_Final', img_path)
+            full_path = os.path.join(self.data_path, 'CelebA_HQ_448_Final/CelebA_HQ_448_Final', img_path)
             image = Image.open(full_path).convert('RGB')
             self.image_cache[img_path] = self.transform(image)
         return self.image_cache[img_path]
