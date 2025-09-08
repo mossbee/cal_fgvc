2fbd045d88ad5b1e12caa82abbf53d7421dc2641
mossbee <hoangnhatdb@gmail.com>
2025-09-08 11:17:21 +0700

    update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Training Config
 ##################################################
 workers = 1                 # number of Dataloader workers
-epochs = 160                # number of epochs
+epochs = 40                # number of epochs
 batch_size = 8             # batch size
 learning_rate = 1e-3        # initial learning rate
 
